ff70e612f1eb45e42c1326e0d2b9a90795e2c564
Vineet <kumvineet30@gmail.com>
2022-06-02 18:22:43 +0530

    added springboot main class

---
+++ src/main/java/com/ms/example/MsInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/example/MsInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
